5c0cb46dd3802e3d679cede8efbc06daf1a5f67c
hooay233 <123224249+hooay233@users.noreply.github.com>
2023-07-09 12:23:59 +0800

    Fix Hoozeeth.plugin.zsh
    Fixed an error in the file.

--- Hoozeeth.plugin.zsh
+++ Hoozeeth.plugin.zsh
@@ -1,3 +1,3 @@
-PROMPT='%F{blue}|%F{magenta}<%B%n%b@%B%m%b>%F{cyan}(%B%D{%y-%m-%d}\b/\B\D{%H:%M:%S}%b)
+PROMPT='%F{blue}|%F{magenta}<%B%n%b@%B%m%b>%F{cyan}(%B%D{%y-%m-%d}%b/%B%D{%H:%M:%S}%b)
 %F{blue}|%F{yellow}[%B%~%b]%F{8}*%?
 %F{blue}| %B%(!.#.$)%b%f [6 q'
